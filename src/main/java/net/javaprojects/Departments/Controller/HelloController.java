caa022889d487f46c0bdad19dc57c3ed96d71e18
K Viddya <62757458+viddya673@users.noreply.github.com>
2023-08-09 15:56:14 +0530

    sample project setup

---
+++ src/main/java/net/javaprojects/Departments/Controller/HelloController.java
@@ -0,0 +1,13 @@
+package net.javaprojects.Departments.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Welcome to my sample application.";
+    }
+}
---
+++ src/test/java/net/javaprojects/Departments/DepartmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaprojects.Departments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
