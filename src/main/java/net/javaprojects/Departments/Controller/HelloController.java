643302ce8f19ccf83e0d09c7228ebf3a1d2bf5a2
K Viddya <62757458+viddya673@users.noreply.github.com>
2023-08-23 17:07:28 +0530

    migrating properties to yml

--- src/main/java/net/javaprojects/Departments/Controller/HelloController.java
+++ src/main/java/net/javaprojects/Departments/Controller/HelloController.java
@@ -1,13 +1,17 @@
 package net.javaprojects.Departments.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     @GetMapping("/")
     public String helloWorld(){
-        return "Welcome to my sample application.";
+        return welcomeMessage;
     }
 }
